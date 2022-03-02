07d994b7bae8ad61ff3a9e76f181e9555fbe84af
julia-podrazka <73885839+julia-podrazka@users.noreply.github.com>
2022-03-02 18:19:04 +0100

    Final version

---
+++ err.h
@@ -0,0 +1,10 @@
+// File provided by the author of a project.
+
+#pragma once
+
+/* wypisuje informacje o błędnym zakończeniu funkcji systemowej
+i kończy działanie */
+extern void syserr(const char* fmt, ...);
+
+/* wypisuje informacje o błędzie i kończy działanie */
+extern void fatal(const char* fmt, ...);
